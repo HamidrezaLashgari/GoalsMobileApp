970337d2e38e599570bf96b884436b84149bc9ad
Hamidreza <hamidlashgari1998@gmail.com>
2022-11-22 16:38:34 +0330

    improve app layout

--- App.js
+++ App.js
@@ -16,7 +16,7 @@ const App = () => {
         <TextInput style={styles.textInput} placeholder="Your course Goal!" />
         <Button title="Add Goal" />
       </View>
-      <View>
+      <View style={styles.goalsContainer}>
         <Text>List of Goals</Text>
       </View>
     </View>
@@ -27,17 +27,27 @@ export default App;
 
 const styles = StyleSheet.create({
   appContainer: {
-    padding: 50,
+    flex: 1,
+    paddingTop: 50,
+    paddingHorizontal: 16,
   },
   inputContainer: {
+    flex: 1,
     flexDirection: 'row',
     justifyContent: 'space-between',
+    alignItems: 'center',
+    marginBottom: 24,
+    borderBottomWidth: 1,
+    borderBottomColor: '#cccccc',
   },
   textInput: {
     borderWidth: 1,
     borderColor: '#cccccc',
-    width: '80%',
+    width: '70%',
     marginRight: 8,
     padding: 8,
+  },
+  goalsContainer: {
+    flex: 5,
   },
 });
