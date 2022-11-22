9d44cdb5bf513b1af131157f909319c07f7bb78b
Hamidreza <hamidlashgari1998@gmail.com>
2022-11-22 16:26:53 +0330

    Change layout with flexbox

--- App.js
+++ App.js
@@ -12,8 +12,8 @@ import {Text, View, StyleSheet, TextInput, Button} from 'react-native';
 const App = () => {
   return (
     <View style={styles.appContainer}>
-      <View>
-        <TextInput placeholder="Your course Goal!" />
+      <View style={styles.inputContainer}>
+        <TextInput style={styles.textInput} placeholder="Your course Goal!" />
         <Button title="Add Goal" />
       </View>
       <View>
@@ -28,5 +28,16 @@ export default App;
 const styles = StyleSheet.create({
   appContainer: {
     padding: 50,
+  },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: '#cccccc',
+    width: '80%',
+    marginRight: 8,
+    padding: 8,
   },
 });
